782c1db6e24e8116cf0e33129269ba123c2dfd9d
Thomas Lisankie <thomas@tomlisankie.com>
2019-03-15 22:26:00 -0400

    Changed Clojure and Overtone versions to be more recent.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject tenor "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [overtone "0.9.1"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [overtone "0.10.3"]])
